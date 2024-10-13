46b6a1bbfda81ad6e240eec263e928f020c5dc38
demonorigine <104482038+demonorigine@users.noreply.github.com>
2024-10-13 15:26:58 +1100

    Début projet

---
+++ script.js
@@ -0,0 +1,28 @@
+async function generateShortLink() {
+    const longUrl = document.getElementById("longUrl").value;
+    const apiUrl = "https://tinyurl.com/api-create.php?url=" + encodeURIComponent(longUrl);
+    console.log(apiUrl);
+    try {
+        const response = await fetch(apiUrl);
+        const shortUrl = await response.text();
+        if(shortUrl){
+            document.getElementById("shortUrl").innerText= shortUrl;
+        }else{
+            throw new Error("Erreur");
+        }
+    }catch (error) {
+        console.error("Erreur", error);
+        document.getElementById("shortUrl").innerText = "Impossible";
+    }
+}
+
+function copyShortUrl(){
+    const shortUrlElement = document.getElementById("shortUrl");
+    const range = document.createRange();
+    range.selectNode(shortUrlElement);
+    window.getSelection().removeAllRanges();
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+    alert("Lien copié : " + shortUrlElement.innerText);
+}
